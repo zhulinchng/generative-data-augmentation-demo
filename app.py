70ca74755c0c5d33c45d49edf7dcef14cacd76be
Zhu Lin Ch'ng <z.chng@liverpool.ac.uk>
2024-06-11 21:59:15 +0800

    update layout

--- app.py
+++ app.py
@@ -50,11 +50,43 @@ def infer(
 ):
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
+    # Input Validation
+    try:
+        assert num_interpolation_steps % 2 == 0
+    except AssertionError:
+        raise ValueError("num_interpolation_steps must be an even number")
+    try:
+        assert sample_mid_interpolation % 2 == 0
+    except AssertionError:
+        raise ValueError("sample_mid_interpolation must be an even number")
+    try:
+        assert remove_n_middle % 2 == 0
+    except AssertionError:
+        raise ValueError("remove_n_middle must be an even number")
+    try:
+        assert num_interpolation_steps >= sample_mid_interpolation
+    except AssertionError:
+        raise ValueError(
+            "num_interpolation_steps must be greater than or equal to sample_mid_interpolation"
+        )
+    try:
+        assert num_interpolation_steps >= 2 and sample_mid_interpolation >= 2
+    except AssertionError:
+        raise ValueError(
+            "num_interpolation_steps and sample_mid_interpolation must be greater than or equal to 2"
+        )
+    try:
+        assert sample_mid_interpolation - remove_n_middle >= 2
+    except AssertionError:
+        raise ValueError(
+            "sample_mid_interpolation must be greater than or equal to remove_n_middle + 2"
+        )
+
     if randomize_seed:
         seed = random.randint(0, MAX_SEED)
     prompts = [prompt1, prompt2]
     generator = torch.Generator().manual_seed(seed)
-    print(seed)
+
     interpolated_prompt_embeds, prompt_metadata = synth.interpolatePrompts(
         prompts,
         pipe,
@@ -65,18 +97,16 @@ def infer(
     )
     negative_prompts = [negative_prompt, negative_prompt]
     if negative_prompts != ["", ""]:
-        interpolated_negative_prompts_embeds, negative_prompt_metadata = (
-            synth.interpolatePrompts(
-                negative_prompts,
-                pipe,
-                num_interpolation_steps,
-                sample_mid_interpolation,
-                remove_n_middle=remove_n_middle,
-                device=device,
-            )
+        interpolated_negative_prompts_embeds, _ = synth.interpolatePrompts(
+            negative_prompts,
+            pipe,
+            num_interpolation_steps,
+            sample_mid_interpolation,
+            remove_n_middle=remove_n_middle,
+            device=device,
         )
     else:
-        interpolated_negative_prompts_embeds, negative_prompt_metadata = [None] * len(
+        interpolated_negative_prompts_embeds, _ = [None] * len(
             interpolated_prompt_embeds
         ), None
 
@@ -129,7 +159,7 @@ def infer(
         * 100
     )
 
-    return image, seed, ssim_score, cosine_sim
+    return image, seed, round(ssim_score, 4), round(cosine_sim, 2)
 
 
 examples1 = [
@@ -141,12 +171,6 @@ examples2 = [
     "A photo of a beagle, a type of dog",
 ]
 
-css = """
-#col-container {
-    margin: 0 auto;
-}
-"""
-
 
 def update_steps(total_steps, interpolation_step):
     if interpolation_step > total_steps:
@@ -154,181 +178,191 @@ def update_steps(total_steps, interpolation_step):
     return gr.update(maximum=total_steps // 2)
 
 
+def update_sampling_steps(total_steps, sample_steps):
+    # if sample_steps > total_steps:
+    #     return gr.update(value=total_steps)
+    return gr.update(value=total_steps)
+
+
 if torch.cuda.is_available():
     power_device = "GPU"
 else:
     power_device = "CPU"
 
-with gr.Blocks(css=css, title="Generative Date Augmentation") as demo:
+with gr.Blocks(title="Generative Date Augmentation") as demo:
 
-    with gr.Column(elem_id="col-container"):
-        gr.Markdown(
-            f"""
-        # Data Augmentation with Image-to-Image Diffusion Models via Prompt Interpolation 
-        Currently running on {power_device}.
+    gr.Markdown(
         """
-        )
+    # Data Augmentation with Image-to-Image Diffusion Models via Prompt Interpolation.
+    """
+    )
+    with gr.Row():
+        with gr.Column():
 
-        input_image = gr.Image(type="pil", label="Image to Augment")
+            input_image = gr.Image(type="pil", label="Image to Augment")
 
-        with gr.Row():
-            prompt1 = gr.Text(
-                label="Prompt 1",
-                show_label=True,
-                max_lines=1,
-                placeholder="Enter your first prompt",
-                container=False,
-            )
-        with gr.Row():
-            prompt2 = gr.Text(
-                label="Prompt 2",
-                show_label=True,
-                max_lines=1,
-                placeholder="Enter your second prompt",
-                container=False,
-            )
-        with gr.Row():
-            gr.Examples(
-                examples=examples1, inputs=[prompt1], label="Example for Prompt 1"
-            )
-            gr.Examples(
-                examples=examples2, inputs=[prompt2], label="Example for Prompt 2"
-            )
-
-        with gr.Row():
-            num_interpolation_steps = gr.Slider(
-                label="Total interpolation steps",
-                minimum=2,
-                maximum=32,
-                step=2,
-                value=16,
-            )
-            interpolation_step = gr.Slider(
-                label="Specific Interpolation Step",
-                minimum=1,
-                maximum=8,
-                step=1,
-                value=8,
-            )
-            num_interpolation_steps.change(
-                fn=update_steps,
-                inputs=[num_interpolation_steps, interpolation_step],
-                outputs=[interpolation_step],
-            )
-            run_button = gr.Button("Run", scale=0)
-
-        result = gr.Image(label="Result", show_label=False)
-
-        with gr.Accordion("Advanced Settings", open=True):
-
-            negative_prompt = gr.Text(
-                label="Negative prompt",
-                max_lines=1,
-                placeholder="Enter a negative prompt",
-                visible=False,
-            )
-
-            seed = gr.Slider(
-                label="Seed",
-                minimum=0,
-                maximum=MAX_SEED,
-                step=1,
-                value=0,
-            )
-
-            randomize_seed = gr.Checkbox(label="Randomize seed", value=True)
-
-            gr.Markdown("Negative Prompt: ")
             with gr.Row():
-                negative_prompt = gr.Text(
-                    label="Negative Prompt",
+                prompt1 = gr.Text(
+                    label="Prompt 1",
                     show_label=True,
                     max_lines=1,
-                    value="blurry image, disfigured, deformed, distorted, cartoon, drawings",
+                    placeholder="Enter your first prompt",
                     container=False,
                 )
             with gr.Row():
-
-                width = gr.Slider(
-                    label="Width",
-                    minimum=256,
-                    maximum=MAX_IMAGE_SIZE,
-                    step=32,
-                    value=512,
+                prompt2 = gr.Text(
+                    label="Prompt 2",
+                    show_label=True,
+                    max_lines=1,
+                    placeholder="Enter your second prompt",
+                    container=False,
                 )
-
-                height = gr.Slider(
-                    label="Height",
-                    minimum=256,
-                    maximum=MAX_IMAGE_SIZE,
-                    step=32,
-                    value=512,
+            with gr.Row():
+                gr.Examples(
+                    examples=examples1, inputs=[prompt1], label="Example for Prompt 1"
+                )
+                gr.Examples(
+                    examples=examples2, inputs=[prompt2], label="Example for Prompt 2"
                 )
 
             with gr.Row():
-
-                guidance_scale = gr.Slider(
-                    label="Guidance scale",
-                    minimum=0.0,
-                    maximum=10.0,
-                    step=0.1,
-                    value=8.0,
-                )
-
-                num_inference_steps = gr.Slider(
-                    label="Number of inference steps",
+                interpolation_step = gr.Slider(
+                    label="Specific Interpolation Step",
                     minimum=1,
-                    maximum=80,
+                    maximum=8,
                     step=1,
-                    value=25,
+                    value=8,
                 )
-            with gr.Row():
-                sample_mid_interpolation = gr.Slider(
-                    label="Number of sampling steps in the middle of interpolation",
+                num_interpolation_steps = gr.Slider(
+                    label="Total interpolation steps",
                     minimum=2,
-                    maximum=80,
+                    maximum=32,
                     step=2,
                     value=16,
                 )
-            with gr.Row():
-                remove_n_middle = gr.Slider(
-                    label="Number of middle steps to remove from interpolation",
+                num_interpolation_steps.change(
+                    fn=update_steps,
+                    inputs=[num_interpolation_steps, interpolation_step],
+                    outputs=[interpolation_step],
+                )
+                run_button = gr.Button("Run", scale=0)
+            with gr.Accordion("Advanced Settings", open=True):
+                negative_prompt = gr.Text(
+                    label="Negative prompt",
+                    max_lines=1,
+                    placeholder="Enter a negative prompt",
+                    visible=False,
+                )
+                seed = gr.Slider(
+                    label="Seed",
                     minimum=0,
-                    maximum=80,
-                    step=2,
+                    maximum=MAX_SEED,
+                    step=1,
                     value=0,
                 )
-        gr.Markdown(
-            """
-            Metadata:
-            """
-        )
-        with gr.Row():
-            show_seed = gr.Label(label="Seed:", value="Randomized seed")
-            ssim_score = gr.Label(label="SSIM Score:", value="Generate to see score")
-            cos_sim = gr.Label(label="CLIP Score:", value="Generate to see score")
-    run_button.click(
-        fn=infer,
-        inputs=[
-            input_image,
-            prompt1,
-            prompt2,
-            negative_prompt,
-            seed,
-            randomize_seed,
-            width,
-            height,
-            guidance_scale,
-            interpolation_step,
-            num_inference_steps,
-            num_interpolation_steps,
-            sample_mid_interpolation,
-            remove_n_middle,
-        ],
-        outputs=[result, show_seed, ssim_score, cos_sim],
-    )
+                randomize_seed = gr.Checkbox(label="Randomize seed", value=True)
+                gr.Markdown("Negative Prompt: ")
+                with gr.Row():
+                    negative_prompt = gr.Text(
+                        label="Negative Prompt",
+                        show_label=True,
+                        max_lines=1,
+                        value="blurry image, disfigured, deformed, distorted, cartoon, drawings",
+                        container=False,
+                    )
+                with gr.Row():
+                    width = gr.Slider(
+                        label="Width",
+                        minimum=256,
+                        maximum=MAX_IMAGE_SIZE,
+                        step=32,
+                        value=512,
+                    )
+                    height = gr.Slider(
+                        label="Height",
+                        minimum=256,
+                        maximum=MAX_IMAGE_SIZE,
+                        step=32,
+                        value=512,
+                    )
+                with gr.Row():
+                    guidance_scale = gr.Slider(
+                        label="Guidance scale",
+                        minimum=0.0,
+                        maximum=10.0,
+                        step=0.1,
+                        value=8.0,
+                    )
+                    num_inference_steps = gr.Slider(
+                        label="Number of inference steps",
+                        minimum=1,
+                        maximum=80,
+                        step=1,
+                        value=25,
+                    )
+                with gr.Row():
+                    sample_mid_interpolation = gr.Slider(
+                        label="Number of sampling steps in the middle of interpolation",
+                        minimum=2,
+                        maximum=80,
+                        step=2,
+                        value=16,
+                    )
+                    num_interpolation_steps.change(
+                        fn=update_sampling_steps,
+                        inputs=[num_interpolation_steps, sample_mid_interpolation],
+                        outputs=[sample_mid_interpolation],
+                    )
+                with gr.Row():
+                    remove_n_middle = gr.Slider(
+                        label="Number of middle steps to remove from interpolation",
+                        minimum=0,
+                        maximum=80,
+                        step=2,
+                        value=0,
+                    )
+        with gr.Column():
+            result = gr.Image(label="Result", show_label=False)
 
-demo.queue().launch()
+            gr.Markdown(
+                """
+                Metadata:
+                """
+            )
+            with gr.Row():
+                show_seed = gr.Label(label="Seed:", value="Randomized seed")
+                ssim_score = gr.Label(
+                    label="SSIM Score:", value="Generate to see score"
+                )
+                cos_sim = gr.Label(label="CLIP Score:", value="Generate to see score")
+            gr.Markdown(
+                f"""
+    Currently running on {power_device}.
+                """
+            )
+
+        run_button.click(
+            fn=infer,
+            inputs=[
+                input_image,
+                prompt1,
+                prompt2,
+                negative_prompt,
+                seed,
+                randomize_seed,
+                width,
+                height,
+                guidance_scale,
+                interpolation_step,
+                num_inference_steps,
+                num_interpolation_steps,
+                sample_mid_interpolation,
+                remove_n_middle,
+            ],
+            outputs=[result, show_seed, ssim_score, cos_sim],
+        )
+
+demo.queue().launch(show_error=True)
 
 """
     input_image,
